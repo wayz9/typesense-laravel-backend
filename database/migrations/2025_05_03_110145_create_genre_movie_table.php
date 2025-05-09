a8cac7f91fff095d81885478854f93a894c144d6
Vukasin <71568418+wayz9@users.noreply.github.com>
2025-05-09 21:40:14 +0200

    wip

--- database/migrations/2025_05_03_110145_create_genre_movie_table.php
+++ database/migrations/2025_05_03_110145_create_genre_movie_table.php
@@ -21,12 +21,4 @@ return new class extends Migration
             $table->foreignIdFor(Movie::class);
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('genre_movie');
-    }
 };
